f7e94404bb94b2ce69631ae3d909acf40a619803
Muneeb Ali <muneeb.ali@teamo.io>
2020-09-08 17:47:36 +0500

    login section and generation JWT

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,24 +1,85 @@
 const bcrypt = require('bcryptjs')
-const { UserInputError } = require('apollo-server')
+const { UserInputError, AuthenticationError } = require('apollo-server')
+const jwt = require('jsonwebtoken')
+const { Op } = require('sequelize')
+
 const { User } = require('../models')
+const { JWT_SECRET } = require('../config/env.json')
 
 module.exports = {
     Query: {
-        getUsers: async () => {
+        getUsers: async (_, __, context) => {
             try {
-                const users = await User.findAll()
+                let user
+                if (context.req && context.req.headers.authorization) {
+                    const token = context.req.headers.authorization.split('Bearer ')[1]
+                    jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+                        if (err) {
+                            throw new AuthenticationError('Unauthenticated')
+                        }
+                        user = decodedToken
+                    })
+                }
+
+                const users = await User.findAll({
+                    // where: { username: { [Op.ne]: user } },
+                })
+
                 return users
+            } catch (err) {
+                console.log(err)
+                throw err
             }
-            catch (err) {
-                console.log(err);
+        },
+        login: async (_, args) => {
+            const { username, password } = args
+            let errors = {}
+
+            try {
+                if (username.trim() === '')
+                    errors.username = 'username must not be empty'
+                if (password === '') errors.password = 'password must not be empty'
+
+                if (Object.keys(errors).length > 0) {
+                    throw new UserInputError('bad input', { errors })
+                }
+
+                const user = await User.findOne({
+                    where: { username },
+                })
+
+                if (!user) {
+                    errors.username = 'user not found'
+                    throw new UserInputError('user not found', { errors })
+                }
+
+                const correctPassword = await bcrypt.compare(password, user.password)
+
+                if (!correctPassword) {
+                    errors.password = 'password is incorrect'
+                    throw new AuthenticationError('password is incorrect', { errors })
+                }
+
+                const token = jwt.sign({ username }, JWT_SECRET, {
+                    expiresIn: 60 * 60,
+                })
+
+                return {
+                    ...user.toJSON(),
+                    createdAt: user.createdAt.toISOString(),
+                    token,
+                }
+            } catch (err) {
+                console.log(err)
+                throw err
             }
         },
     },
-
     Mutation: {
         register: async (_, args) => {
             let { username, email, password, confirmPassword } = args
             let errors = {}
+
             try {
                 // Validate input data
                 if (email.trim() === '') errors.email = 'email must not be empty'
@@ -28,34 +89,44 @@ module.exports = {
                     errors.password = 'password must not be empty'
                 if (confirmPassword.trim() === '')
                     errors.confirmPassword = 'repeat password must not be empty'
-                if (password !== confirmPassword) errors.confirmPassword = 'password must match'
 
-                // check if username/email is exists
+                if (password !== confirmPassword)
+                    errors.confirmPassword = 'passwords must match'
+
+                // // Check if username / email exists
                 // const userByUsername = await User.findOne({ where: { username } })
                 // const userByEmail = await User.findOne({ where: { email } })
-                // if (userByUsername) errors.username = "Username is taken"
-                // if (userByEmail) errors.email = "Email is taken"
+
+                // if (userByUsername) errors.username = 'Username is taken'
+                // if (userByEmail) errors.email = 'Email is taken'
 
                 if (Object.keys(errors).length > 0) {
                     throw errors
                 }
-                // hash passwords
-                password = await bcrypt.hash(password, 6);
-                //create user
+
+                // Hash password
+                password = await bcrypt.hash(password, 6)
+
+                // Create user
                 const user = await User.create({
-                    username, email, password
+                    username,
+                    email,
+                    password,
                 })
+
                 // Return user
                 return user
             } catch (err) {
                 console.log(err)
-                if (err.name === "SequelizeUniqueConstraintError") {
-                    err.errors.forEach((e) => (errors[e.path] = `${e.path} is already taken`))
+                if (err.name === 'SequelizeUniqueConstraintError') {
+                    err.errors.forEach(
+                        (e) => (errors[e.path] = `${e.path} is already taken`)
+                    )
                 } else if (err.name === 'SequelizeValidationError') {
                     err.errors.forEach((e) => (errors[e.path] = e.message))
                 }
-                throw new UserInputError('Bad Input', { errors })
+                throw new UserInputError('Bad input', { errors })
             }
-        }
-    }
+        },
+    },
 }
